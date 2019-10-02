f08866e082102df41b193636a54389947a652dab
spshreena <54719859+spshreena@users.noreply.github.com>
2019-10-02 10:27:38 -0400

    Update Script2.py

--- Script2.py
+++ Script2.py
@@ -1,1 +1,38 @@
 # Place necessary comments and code here. 
+area = [100,200,300]
+area_inches = [i * 12 for i in area]
+print(area_inches)
+
+for i in area_inches:
+    rainfall_inches_is = area*1
+    rainfall_gallon_is = (rainfall_inches_is / gal_convert)
+
+
+'''
+plot_length_is = 50 *12
+
+
+plot_width_is = 20 *12
+
+
+#calculate Area
+area = plot_length_is * plot_width_is
+
+
+#calculating rainfall in inches
+rainfall_inches_is = area*1  
+
+#conversion value 
+gal_convert = 231
+
+#rainfall in gallon 
+rainfall_gallon_is = (rainfall_inches_is / gal_convert)
+
+
+#Total runoff in gallons
+print ('plot_length_is', plot_length_is)
+print('plot_width_is', 
+plot_width_is)
+print ('area', area)
+print (' rainfall_gallon_is', rainfall_gallon_is)
+'''
