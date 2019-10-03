3d08fc25e522ce0b631a622e70eaaa98023bc46b
spshreena <54719859+spshreena@users.noreply.github.com>
2019-10-03 17:46:05 -0400

    Update Script2.py

--- Script2.py
+++ Script2.py
@@ -40,17 +40,55 @@ def plot_output(ask_user):
 #call the function with arguement as ask_user
 plot_output(ask_user)
 # here is where we get an EOF error, except the else past of the if statement works 
+#-------------------------------------------------------------------------------------------------------------------------------
+#version 2 of code
 
-#_____________________________________
-#new code
-area = [100,200,300]
-area_inches = [i * 12 for i in area]
-print(area_inches)
+area_list = [14400, 28800, 43200]
+print "Plot 1 area is: ", + area_list[0]
+print "Plot 2 area is: ", + area_list[1]
+print "Plot 3 area is: ", + area_list[2]
+#print "plot_length is: ", + length, "inches"
+#print "plot_width is: ", + width, "inches"
 
-for i in area_inches:
-    rainfall_inches_is = area*1
-    rainfall_gallon_is = (rainfall_inches_is / gal_convert)
-#test
+#now convert to inches because rainwater is measured in inches
+# Since you are converting area from feet sq to inches sq, you want to multiply the area value by 144 instead of 12.
+area_inches = [i * 1 for i in area_list]
+print (area_inches)
+
+rainfall_inches = 1
+runoff_inches = [i * rainfall_inches for i in area_inches]
+print runoff_inches
+
+#convert cubic inches to gallons: divide the volume value by 231
+
+runoff_gallons = [i/ 231 for i in runoff_inches]
+#print "runoff_gallons is: ", +  runoff_gallons
+print runoff_gallons
+print "Plot 1 rainfall is: ", + runoff_gallons[0],"gallons"
+print "Plot 2 rainfall is: ", + runoff_gallons[1],"gallons"
+print "Plot 3 rainfall is: ", + runoff_gallons[2],"gallons"
+   
+#asking the user what plot they would like to know the runoff for
+
+def ask_user_():
+    ask_user = raw_input("which plot would you like to know the runoff for?")
+    if ask_user == "plot 1":
+        print runoff_gallons[0]
+    elif ask_user == "plot 2":
+         print runoff_gallons[1]
+    elif ask_user == "plot 3":
+        print runoff_gallons[2]
+    #after finding out the runoff for one plot keep going to find the runoff for next plot 
+    cont= raw_input('do you want to fing the runoff for another plot y/n')
+    if cont == 'y':
+        ask_user_()
+    else:
+        print 'done' 
+    
+print ask_user_()
+
+
+
 
 '''
 plot_length_is = 50 *12
