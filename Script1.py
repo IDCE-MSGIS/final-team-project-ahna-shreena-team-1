01ee66d75bc1a3887cd8ef1753a8961ab6167372
spshreena <54719859+spshreena@users.noreply.github.com>
2019-10-04 12:32:08 -0400

    Update Script1.py

--- Script1.py
+++ Script1.py
@@ -1,10 +1,11 @@
 '''
+#Name: Ahna Knudsen and Shreena Pyakurel
 # Assignment title: Final Project- Web-scraping Weather Forecast
-# Date: 09/23/2019
+# Date: 10/04/2019
 # Description: The script web-scrapes the weather.gov website to extract the 5-Day weather forecast for a given location
 # Inputs: Latitude & Longitude in Decimal Degrees
 # Outputs: 5-Day Weather Forecast
-# Time:
+# Time:90 minutes
 '''
 
 # import required libraries
@@ -16,80 +17,15 @@ forecast = []
 
 ## Provide the latitude and longitude for the location you would like to check the forecast for
 ## Lat/lon in decimal degrees provided for Worcester, MA
-lat = str(input("Latitiude of your choice within the US: "))
-lon = str(input("Longitude of your choice within the US: "))
-
-# Create url for the requested location through string concatenation
-url = 'https://forecast.weather.gov/MapClick.php?lat='+lat+"&lon="+lon
-# Check if the URL exists
-# print url
-
-# Send request to retrieve the web-page using the get() function from the requests library
-# The page variable stores the response from the web-page
-page = requests.get(url)
-
-# Create a BeautifulSoup object with the response from the URL
-# Access contents of the web-page using .content
-# html_parser is used since our page is in HTML format
-soup=BeautifulSoup(page.content,"html.parser")
-
-# Locate elements on page to be scraped
-# findAll() locates all occurrences of div tag with the given class name
-# stores it in the BeautifulSoup object
-weather_forecast = soup.findAll("li", {"class": "forecast-tombstone"})
-
-# Loop through the BeautifulSoup object to extract text text from every class instance using .text
-# Store results in a list
-for i in weather_forecast:
-    i = i.text
-    forecast.append(i)
-
-# Print list to remove unicode characters
-#for day in forecast:
- #   print day
-
-#code from lab 6
-# Split string into a list
-# Use two blank lines (\n\n) as the separator
-# Creates a list item at every instance of separator
-forecast_list = forecast.split('\n\n')
-
-# Loop through list to make string replacements to each item
-# Remove extra whitespaces or lines for a cleaner format
-for day in forecast_list:
-    day = day.replace('\n',': ')
-    day = day.replace('High', ' High')
-    day = day.replace('Sunny', 'Sunny, ')
-    day = day.replace('Clear', 'Clear, ')
-    day = day.replace('Sunny,  thenChanceShowers', 'Sunny then Chance Showers,')
-    print day
-#------------------------------------------------------------------------------------------------------------
-'''
-# Assignment title: Final Project- Web-scraping Weather Forecast
-# Date: 09/23/2019
-# Description: The script web-scrapes the weather.gov website to extract the 5-Day weather forecast for a given location
-# Inputs: Latitude & Longitude in Decimal Degrees
-# Outputs: 5-Day Weather Forecast
-# Time:
-'''
-
-# import required libraries
-import requests
-from bs4 import BeautifulSoup
-
-# List to store response
-forecast = []
-
-## Provide the latitude and longitude for the location you would like to check the forecast for
-## Lat/lon in decimal degrees provided for Worcester, MA
-#we are using str 
+#add in user input to specify latitude and longitude
+#use the str function to convert latitude values into strings type 
 lat = str(input("Latitiude of your choice within the US: "))
 lon = str(input("Longitude of your choice within the US: "))
 
 
 # Create url for the requested location through string concatenation
 url = 'https://forecast.weather.gov/MapClick.php?lat='+lat+"&lon="+lon
-# Check if the URL exists
+# Check if the URL exists- incremental testing
 print (url)
 
 # Send request to retrieve the web-page using the get() function from the requests library
@@ -105,8 +41,6 @@ soup=BeautifulSoup(page.content,"html.parser")
 # findAll() locates all occurrences of div tag with the given class name
 # stores it in the BeautifulSoup object
 weather_forecast = soup.findAll("li", {"class": "forecast-tombstone"})
-
-
 # Loop through the BeautifulSoup object to extract text text from every class instance using .text
 # Store results in a list
 for i in weather_forecast:
@@ -115,21 +49,17 @@ for i in weather_forecast:
 
 
 # Print list to remove unicode characters
+#used forloop to iterate through forcast and used .replace function to clean up spacing in output
 for day in forecast:
+ # day = day.replace('\n',)
+ # day = day.replace('High', ' High') #tried different spacing options
+  day = day.replace('Sunny', 'Sunny, ')
+  day = day.replace('Clear', 'Clear, ')
+  day = day.replace('Sunny,', 'Sunny, ')
+  day = day.replace('Patchy', 'Patchy ')
+  day = day.replace('Frost', 'Frost ')
+  day = day.replace('Likely', 'Likely ')
+  day = day.replace('Chance', 'Chance ' )
+  day = day.replace('Cloudy', 'Cloudy ')
+  day = day.replace('Mostly', 'Mostly ')
   print day
-
-#code from lab 6
-# Split string into a list
-# Use two blank lines (\n\n) as the separator
-# Creates a list item at every instance of separator
-forecast_list = forecast.split('\n\n')
-print(forecast_list)
-# Loop through list to make string replacements to each item
-# Remove extra whitespaces or lines for a cleaner format
-for day in forecast_list:
-    day = day.replace('\n',': ')
-    day = day.replace('High', ' High')
-    day = day.replace('Sunny', 'Sunny, ')
-    day = day.replace('Clear', 'Clear, ')
-    day = day.replace('Sunny,  thenChanceShowers', 'Sunny then Chance Showers,')
-    print day
