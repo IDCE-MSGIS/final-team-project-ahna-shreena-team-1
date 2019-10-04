57bfc6244a67b32672b0196019b65a793d60e177
spshreena <54719859+spshreena@users.noreply.github.com>
2019-10-04 11:37:46 -0400

    Update Script1.py

--- Script1.py
+++ Script1.py
@@ -63,3 +63,73 @@ for day in forecast_list:
     day = day.replace('Clear', 'Clear, ')
     day = day.replace('Sunny,  thenChanceShowers', 'Sunny then Chance Showers,')
     print day
+#------------------------------------------------------------------------------------------------------------
+'''
+# Assignment title: Final Project- Web-scraping Weather Forecast
+# Date: 09/23/2019
+# Description: The script web-scrapes the weather.gov website to extract the 5-Day weather forecast for a given location
+# Inputs: Latitude & Longitude in Decimal Degrees
+# Outputs: 5-Day Weather Forecast
+# Time:
+'''
+
+# import required libraries
+import requests
+from bs4 import BeautifulSoup
+
+# List to store response
+forecast = []
+
+## Provide the latitude and longitude for the location you would like to check the forecast for
+## Lat/lon in decimal degrees provided for Worcester, MA
+#we are using str 
+lat = str(input("Latitiude of your choice within the US: "))
+lon = str(input("Longitude of your choice within the US: "))
+
+
+# Create url for the requested location through string concatenation
+url = 'https://forecast.weather.gov/MapClick.php?lat='+lat+"&lon="+lon
+# Check if the URL exists
+print (url)
+
+# Send request to retrieve the web-page using the get() function from the requests library
+# The page variable stores the response from the web-page
+page = requests.get(url)
+
+# Create a BeautifulSoup object with the response from the URL
+# Access contents of the web-page using .content
+# html_parser is used since our page is in HTML format
+soup=BeautifulSoup(page.content,"html.parser")
+
+# Locate elements on page to be scraped
+# findAll() locates all occurrences of div tag with the given class name
+# stores it in the BeautifulSoup object
+weather_forecast = soup.findAll("li", {"class": "forecast-tombstone"})
+
+
+# Loop through the BeautifulSoup object to extract text text from every class instance using .text
+# Store results in a list
+for i in weather_forecast:
+    i = i.text
+    forecast.append(i)
+
+
+# Print list to remove unicode characters
+for day in forecast:
+  print day
+
+#code from lab 6
+# Split string into a list
+# Use two blank lines (\n\n) as the separator
+# Creates a list item at every instance of separator
+forecast_list = forecast.split('\n\n')
+print(forecast_list)
+# Loop through list to make string replacements to each item
+# Remove extra whitespaces or lines for a cleaner format
+for day in forecast_list:
+    day = day.replace('\n',': ')
+    day = day.replace('High', ' High')
+    day = day.replace('Sunny', 'Sunny, ')
+    day = day.replace('Clear', 'Clear, ')
+    day = day.replace('Sunny,  thenChanceShowers', 'Sunny then Chance Showers,')
+    print day
